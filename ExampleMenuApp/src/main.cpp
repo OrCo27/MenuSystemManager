91c6780232f47384ce627637947af6f1c1b7d8d3
Or Cohen <or.cohen22@gmail.com>
2021-09-28 16:19:12 +0300

    add comments on example main

--- ExampleMenuApp/src/main.cpp
+++ ExampleMenuApp/src/main.cpp
@@ -4,7 +4,7 @@
 #include "ScreenStub.h"
 #include "KeyBoardStub.h"
 
-class Operation : public IOperationItem
+class ExampleOperation : public IOperationItem
 {
 public:
     void doAction() override
@@ -15,45 +15,51 @@ public:
 
 ScreenStub screen;
 KeyBoardStub  keyboard;
-
+ExampleOperation exOp;
 shared_ptr<Navigation> nav;
-Operation op1;
 
 void setup()
 {
     MenuBuilder mb(keyboard, screen);
-    mb.registerUpKey('W')
-        .registerDownKey('S')
-        .registerBackKey('B')
-        .registerEnterKey('E');
 
+    /* register all characters to required commands */
+    mb.registerUpKey('W')
+      .registerDownKey('S')
+      .registerBackKey('B')
+      .registerEnterKey('E');
+
+    /* create all menu required */
     Menu* main = mb.addMenu("Main-Menu");
     Menu* m1 = mb.addMenu("Sub-Menu1");
     Menu* m2 = mb.addMenu("Sub-Menu2");
 
+    /* define the initial menu */
     mb.setInitialMenu(main);
 
+    /* add items for [Main-Menu] */
     mb.addMenuItem(main, "m1", m1);
     mb.addMenuItem(main, "m2", m2);
-    mb.addOperationItem(main, "m3", &op1);
-    mb.addOperationItem(main, "m4", &op1);
-    mb.addOperationItem(main, "m5", &op1);
-    mb.addOperationItem(main, "m6", &op1);
+    mb.addOperationItem(main, "m3", &exOp);
+    mb.addOperationItem(main, "m4", &exOp);
+    mb.addOperationItem(main, "m5", &exOp);
+    mb.addOperationItem(main, "m6", &exOp);
 
-    mb.addOperationItem(m1, "m11", &op1);
-    mb.addOperationItem(m1, "m12", &op1);
+    /* add items for [Sub-Menu1] */
+    mb.addOperationItem(m1, "m11", &exOp);
+    mb.addOperationItem(m1, "m12", &exOp);
 
+    /* add items for [Sub-Menu2] */
     mb.addStateSelectionItem(m2, "Time")
         .addState("10ms")
         .addState("20ms")
         .addState("30ms")
         .addState("40ms");
-
     mb.addStateSelectionItem(m2, "Option")
         .addState("Regular")
         .addState("Medium")
         .addState("Hard");
 
+    /* get the final instance */
     nav = mb.getInstance();
 }
 
